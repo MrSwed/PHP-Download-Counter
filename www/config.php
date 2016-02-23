931844d12d3ce395a52d6adf9609810dd1277076
MrSwed <webmaster@sdcollection.com>
2016-02-23 09:45:49 +0300

    prevent errors - on mobile device may be no http_referrer

--- www/config.php
+++ www/config.php
@@ -12,5 +12,5 @@ $nixWithExec = true; //Set to true if server is running on a *nix system with PH
 
 date_default_timezone_set('America/New_York'); //List of time zones: http://www.php.net/manual/en/timezones.php
 
-$clientInfo = $_SERVER['REMOTE_ADDR'] . " - [" . date('j/M/Y:G:i:s T')  . "] - '" . $_SERVER['HTTP_USER_AGENT'] . "' - " . $_SERVER['HTTP_REFERER'] . "\n"; 
+$clientInfo = @$_SERVER['REMOTE_ADDR'] . " - [" . date('j/M/Y:G:i:s T')  . "] - '" . @$_SERVER['HTTP_USER_AGENT'] . "' - " . @$_SERVER['HTTP_REFERER'] . "\n"; 
 ?>
